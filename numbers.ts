0ae0d6ab63068ffac7a1cabfd8d9a9d217a41b60
Etienne Cointet <etienne@cointet.com>
2021-08-30 10:56:42 +0200

    update

--- numbers.ts
+++ numbers.ts
@@ -9,11 +9,25 @@ export class AppController {
 
     @Get('/outbound')
     sendSMS() {
+        const client = new plivo.Client(process.env.AUTH_ID, process.env.AUTH_TOKEN);
         console.log("==========");
         console.log("/!\ Phone Number: "+process.env.DEST);
         console.log("/!\ Message: "+process.env.MESSAGE);
         console.log("==========");
         console.log("For security reasons, we are not sending the SMS in this version. This is only for testing purpose.");
         console.log("Congrats, you finished the lab!");
+        client.messages.create(
+                process.env.SOURCE,
+                //process.env.DEST,
+                process.env.MESSAGE,
+            )
+            .then(
+                function(response) {
+                    console.log(response);
+                },
+                function(err) {
+                    console.error(err);
+                },
+            );
     }
 }
